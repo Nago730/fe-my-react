952556a58c810ff8c1cb11c89c1637466fe8825f
Nago730 <jqk1797@gmail.com>
2025-06-24 19:22:48 +0900

    feat: TestApp 컴포넌트 추가 및 상태 관리 기능 구현

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -1,5 +1,16 @@
 import { render } from '@my-react/core/render';
+import { useState } from '../../src/runtime/useState';
 import App from './App';
 
 const root = document.getElementById('root');
-if (root) render(<App />, root);
+if (root) render(<TestApp />, root);
+
+function TestApp() {
+	const [count, setCount] = useState(0);
+	return (
+		<div>
+			<span>{count}</span>
+			<button onclick={(prev) => setCount(prev + 1)}>버튼</button>
+		</div>
+	);
+}
