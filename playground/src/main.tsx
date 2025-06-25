da80bf955b8ea4273f65334dae4475062c4793e9
Nago730 <jqk1797@gmail.com>
2025-06-25 12:46:45 +0900

    feat: TestApp 컴포넌트에 모달 기능 추가 및 내부 카운터 컴포넌트 구현

--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -7,11 +7,34 @@ if (root) render(<TestApp />, root);
 
 function TestApp() {
 	const [count, setCount] = useState(0);
+	const [open, setOpen] = useState(false);
+
 	const handle = () => setCount((prev) => prev + 1);
+	const handleModal = () => setOpen((prev) => !prev);
+
 	return (
 		<div>
 			<span>{count}</span>
 			<button onclick={handle}>버튼</button>
+			<button onclick={handleModal}>모달토글버튼</button>
+			<InnerCounter />
+			{open && <Modal count={count} />}
 		</div>
 	);
+}
+
+function InnerCounter() {
+	const [count, setCount] = useState(0);
+	const handle = () => setCount((prev) => prev + 1);
+
+	return (
+		<div>
+			<span>{count}</span>
+			<button onclick={handle}>내부버튼</button>
+		</div>
+	);
+}
+
+function Modal({ count }) {
+	return <div>{`모달 - 현재 카운트: ${count}`}</div>;
 }
